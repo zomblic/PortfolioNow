e9fe81decceaa2702d28c7784488f6accba30d3d
zomblic <zomblic@hotmail.com>
2025-03-22 20:39:58 -0400

    updated css + avatar

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,16 @@
 import "./About.css";
+import zomblic from "../assets/images/zomblic.png";
 
 export default function About() {
   return (
     <div className="cyberpunk-container">
     <h1 className="cyberpunk-title">Intro to a Zomblic</h1>
     <p className="cyberpunk-text">
+
+    
+      <img src={zomblic} alt="Zomblic" className="zombie-gif" />
+      <br />
+      <br />
       Zomblic is a sleep-deprived zombie who is learning about the world of coding. While their ADHD finds all the different colors amusing, they struggle with retaining information—as they are a zombie. 
       <br /><br />
       Zomblic last tested their general writing speed and was satisfied with reaching the 100-word area. However, they are currently working on coding typing speed. This causes their fingers to get lost on the keyboard.
